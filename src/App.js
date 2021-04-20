92cbb4763066e088f6e9656b517d0eca1efa8c99
Joustin Ortiz <ortizjoustin@gmail.com>
2021-04-19 22:18:58 -0500

    Primer commit, react router puesto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./style.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Carta from "./components/Carta";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Router>
+          <div className="bg-green-700">
+            <Link to="/">Inicio</Link>
+            <Link to="/carta">Carta</Link>
+          </div>
+
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <Home></Home>;
+            }}
+          />
+
+          <Route
+            exact
+            path="/carta"
+            render={() => {
+              return <Carta></Carta>;
+            }}
+          />
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/components/Carta.js
@@ -0,0 +1,9 @@
+function Carta() {
+  return (
+    <div>
+      <h1>Esta es la pagina de carta</h1>
+    </div>
+  );
+}
+
+export default Carta;
